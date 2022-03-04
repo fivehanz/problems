3b0d3006b6bf90e0029bbd4c83e8b3ef0bf3ec2a
Hanz <haniel56@zoho.eu>
2022-03-04 16:49:27 +0530

    fix: type and improved code

--- 167.go
+++ 167.go
@@ -5,17 +5,18 @@
  */
 
 func twoSum(numbers []int, target int) []int {
-	var start int
+	var start, sum int
 	var end int = len(numbers) - 1
 
 	indices := make([]int, 2)
 
 	for start < end {
-		if numbers[start]+numbers[end] < target {
+		sum = numbers[start] + numbers[end]
+		if sum < target {
 			start++
-		} else if numbers[start]+numbers[end] > target {
+		} else if sum > target {
 			end--
-		} else if numbers[start]+numbers[end] == target {
+		} else if sum == target {
 			indices[0] = start + 1
 			indices[1] = end + 1
 			break
